683c0d11cb4f46a6d24ef8d19ce69e76898c3526
zzzz <QijingZeng02@163.com>
2025-01-10 22:38:58 +0000

    Remove login functionality and related files

--- Website/front-end/src/store/modules/user.js
+++ Website/front-end/src/store/modules/user.js
@@ -1,45 +1,23 @@
 const state = {
-  token: 'mock-token',
-  name: 'Mock User',
-  avatar: '',
-  introduction: '',
+  name: 'User',
   roles: ['admin']
 }
 
 const mutations = {
-  SET_TOKEN: (state, token) => {
-    state.token = token
-  },
   SET_ROLES: (state, roles) => {
     state.roles = roles
   }
 }
 
 const actions = {
-  // 登录
-  login({ commit }) {
-    commit('SET_TOKEN', 'mock-token')
-    return Promise.resolve()
-  },
-
-  // 获取用户信息
   getInfo({ commit }) {
     commit('SET_ROLES', ['admin'])
     return Promise.resolve({
       data: {
         roles: ['admin'],
-        name: 'Mock User',
-        avatar: '',
-        introduction: ''
+        name: 'User'
       }
     })
-  },
-
-  // 登出
-  logout({ commit }) {
-    commit('SET_TOKEN', '')
-    commit('SET_ROLES', [])
-    return Promise.resolve()
   }
 }
 
