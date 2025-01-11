a6dd18b4fdd40245f5c176b284285a86110c1a41
Steak Fairy <wangqijie77@gmail.com>
2025-01-11 03:05:49 +0000

    Add about

---
+++ Website/front-end/src/views/about/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about-page bg-black">
+    <TheNavbar />
+    <BookingBanner />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar.vue'
+import TheFooter from '@/components/TheFooter.vue'
+import BookingBanner from '@/components/BookingBanner.vue'
+
+export default {
+  name: 'AboutPage',
+  components: {
+    TheNavbar,
+    TheFooter,
+    BookingBanner
+  }
+}
+</script>
+
+<style scoped>
+.about-page {
+  min-height: 100vh;
+}
+</style>
